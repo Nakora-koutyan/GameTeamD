65ea02f7baf9cd076b7d14716722e7ce30d53691
Nakora-koutyan <104492122+Nakora-koutyan@users.noreply.github.com>
2023-04-26 19:31:49 +0900

    player表示

--- GameTeamD/GameMain/Player.h
+++ GameTeamD/GameMain/Player.h
@@ -1,15 +1,39 @@
 #pragma once
+#include "DxLib.h"
+
+#define BUTTONS 16
+
+//スティック
+struct Stick
+{
+	short ThumbX;	//横軸値
+};
+
 class C_PLAYER
 {
-public:
-
-	int gOldKey;           // 前回の入力キー
+private:
+	int gOldKey[BUTTONS];           // 前回の入力キー
 	int gNowKey;           // 今回の入力キー
 	int gKeyFlg;           // 入力キー情報
 
 
-	int gPlayer_x = 640;
+	int gPlayer_x;
 
+	int gPlayerimages;
+public:
+	C_PLAYER()
+	{
+		gOldKey = 0;
+		gNowKey = 0;
+		gKeyFlg = 0;
 
-	void input();
+		gPlayer_x = 640;
+		gPlayerimages = LoadGraph("Material/Images/characterWalk02.png");
+	}
+	~C_PLAYER()
+	{
+
+	}
+	void Update();
+	void Draw() const;
 };
