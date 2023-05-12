075afa1313ed3da85449bb64199849eec6924797
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2023-05-12 10:23:54 +0900

    Update Ranking.cpp

--- GameTeamD/Ranking/Ranking.cpp
+++ GameTeamD/Ranking/Ranking.cpp
@@ -28,5 +28,56 @@ void Ranking::DrawRanking(void)
 			gRanking[i].score);
 	}
 	DrawString(100, 450, "--- スペースキーを押してタイトルへ戻る ---", 0xff0000, 0);
+}
 
+/****************************************
+* ランキングデータの保存
+*****************************************/
+int SaveRanking(void)
+{
+	FILE* fp;
+#pragma warning(disable:4996)
+
+	//ファイルオープン
+	if ((fp = fopen("dat/rankingdata.txt", "w")) == NULL) {
+		/*エラー処理*/
+		printf("Ranking Data Error\n");
+		return -1;
+	}
+
+	//ランキングデータ分配列データを書き込む
+	for (int i = 0; i < RANK_MAX; i++) {
+		fprintf(fp, "%2d %10s %10d\n", gRanking[i].no, gRanking[i].name,
+			gRanking[i].score);
+	}
+
+	//ファイルクローズ
+	fclose(fp);
+	return 0;
+}
+
+/****************************************
+* ランキングデータ読み込み
+*****************************************/
+int ReadRanking(void)
+{
+	FILE* fp;
+#pragma warning(disable:4996)
+
+	//ファイルオープン
+	if ((fp = fopen("dat/rankingdata.txt", "r")) == NULL) {
+		//エラー処理
+		printf("Ranking Data Error\n");
+		return -1;
+	}
+
+	//ランキングデータ分配列データを読み込む
+	for (int i = 0; i < RANK_MAX; i++) {
+		int dammy = fscanf(fp, "%2d %10s %10d", &gRanking[i].no, gRanking[i].name, &gRanking[i].score);
+	}
+
+	//ファイルクローズ
+	fclose(fp);
+
+	return 0;
 }
