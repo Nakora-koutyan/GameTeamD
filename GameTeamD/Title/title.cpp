45f9528e0e3ab899fc31a2aafb767eb1bdf2d8a8
taiki0627 <104492163+taiki0627@users.noreply.github.com>
2023-05-15 09:54:53 +0900

    PadInput

--- GameTeamD/Title/title.cpp
+++ GameTeamD/Title/title.cpp
@@ -6,7 +6,14 @@
 
 void TITLE::print()
 {	
-	
+	if (g_KeyFlg & PAD_INPUT_DOWN) {
+		if (++g_MenuNumber > 3) g_MenuNumber = 0;
+		//PlaySoundMem(Menu1, DX_PLAYTYPE_BACK);
+	}
+	if (g_KeyFlg & PAD_INPUT_UP) {
+		if (--g_MenuNumber < 0) g_MenuNumber = 2;
+		//PlaySoundMem(Menu1, DX_PLAYTYPE_BACK);
+	}
 	//PlaySoundMem(gMainBGM, DX_PLAYTYPE_LOOP);
 
 }
--- GameTeamD/Title/title.h
+++ GameTeamD/Title/title.h
@@ -1,6 +1,18 @@
 #pragma once
 class TITLE
 {
+private:
+	enum mode {
+		E_TITLE,
+		E_INIT,
+		E_MAIN,
+		E_RANKING,
+		E_HELP,
+		E_INPUTNAME,
+		E_RESULT,
+		E_END,
+		E_CLOSE
+	};
 public:
 
 
