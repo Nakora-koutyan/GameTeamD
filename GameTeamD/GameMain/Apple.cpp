c2f341bbe78600d25a2851fc6d1154d756eb663d
Kotoribako <youto-tome-k22h@stu.kbc.ac.jp>
2023-05-16 16:56:04 +0900

    a

--- GameTeamD/GameMain/Apple.cpp
+++ GameTeamD/GameMain/Apple.cpp
@@ -30,14 +30,6 @@ int APPLE::IMAGES_RINGO()
 	DrawGraph(300, 200, M.gApple[2], TRUE);
 	DrawGraph(400, 200, M.gApple[3], TRUE);
 
-	
-	/*int red, blue, gold, poison;
-	red = DrawCircle(0, 0, 80, 0xff0000, TRUE);
-	blue = DrawCircle(100, 100, 80, 0x00ff00, TRUE);
-	gold = DrawCircle(200, 200, 80, 0xffd700, TRUE);
-	poison = DrawCircle(300, 300, 80, 0xff00ff, TRUE);
-	*/
-
 	AppleControl();
 	
 	return 0;
